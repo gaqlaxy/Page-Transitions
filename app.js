c7f0a8ff7f509255e26c50fed7fd67a272014c66
gaqlaxy <v4vripper@gmail.com>
2024-07-08 07:01:00 +0530

    Added Up and down animation

---
+++ app.js
@@ -0,0 +1,86 @@
+const tlLeave = gsap.timeline({
+    defaults: { duration: 0.75, ease: "Power2.easeOut" },
+});
+const tlEnter = gsap.timeline({
+    defaults: { duration: 0.75, ease: "Power2.easeOut" },
+});
+
+const leaveAnimation = (current, done) => {
+    const product = current.querySelector('.image-container');
+    let text = current.querySelector('.showcase-text');
+    let circles = current.querySelector('.circle');
+    let arrow = current.querySelector('.showcase-arrow');
+    return (
+        tlLeave.fromTo(arrow, { opacity: 1, y: 0 }, { opacity: 0, y: 50 }),
+        tlLeave.fromTo(product, { y: 0, opacity: 1 }, { y: 100, opacity: 0, onComplete: done }, "<"),
+        tlLeave.fromTo(text, 
+            { y: 0, opacity: 1 }, 
+            { opacity: 0, y: 100 }, 
+            "<"
+        ),
+        tlLeave.fromTo(
+            circles,
+            {y: 0, opacity:1},
+            {y: -200,
+                opacity:0,
+                stagger: 0.15,
+                ease: 'back.out(1.7)',
+                duration:1,
+            },
+            "<",
+        )
+    );
+};
+
+
+
+const enterAnimation = (current, done) => {
+    const product = current.querySelector('.image-container');
+    let text = current.querySelector('.showcase-text');
+    let circles = current.querySelector('.circle');
+    let arrow = current.querySelector('.showcase-arrow');
+    return (
+        tlLeave.fromTo(arrow, { opacity: 0, y: 50 }, { opacity: 1, y: 0 }),
+        tlLeave.fromTo(product, { y: -100, opacity: 0 }, { y: 0, opacity: 1, onComplete: done }, "<"),
+        tlLeave.fromTo(text, 
+            { y: 100, opacity: 0 }, 
+            { opacity: 1, y: 0 }, 
+            "<"
+        ),
+        tlLeave.fromTo(
+            circles,
+            {y: -200, opacity:0},
+            {y: 0,
+                opacity:1,
+                stagger: 0.15,
+                ease: 'back.out(1.7)',
+                duration:1,
+            },
+            "<",
+        )
+    );
+};
+
+
+
+// Run animation 
+
+barba.init({
+    preventRunning: true,
+    transitions: [
+
+        {
+            name: "default",
+            leave(data) {
+                const done = this.async();
+                let current = data.current.container;
+                leaveAnimation(current, done)
+            },
+            enter(data) {
+                const done = this.async();
+                let next = data.next.container;
+                enterAnimation(next, done);
+            }
+        }
+    ]
+})
